c22dade8a779f4dcf40d598fa85d209a2d9a553b
priyashanand <tomriddlealbussirius@gmail.com>
2024-09-24 18:09:03 +0530

    complete login and signup done

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -25,6 +25,13 @@ exports.signup = async (req, res, next) => {
             status: 'success',
             message: 'User registered sucessfully',
             token,
+            user:{
+                _id: newUser._id,
+                name: newUser.name,
+                email: newUser.email,
+                role: newUser.role
+
+            }
         })
 
     } catch(error){
@@ -33,5 +40,34 @@ exports.signup = async (req, res, next) => {
 }
 
 exports.login = async (req, res, next) => {
-    
+    try{
+        const {email, password} = req.body;
+        const user = await User.findOne({email});
+
+        if(!user) return next(new createError('User not found', 404));
+        
+        const isPasswordValid = bcrypt.compare(password, user.password)
+
+        if(!isPasswordValid) return next(new createError('Incorrect password', 401));
+
+        const token = jwt.sign({_id: user._id}, 'secretkey123',{
+            expiresIn: '1d',
+        });
+
+        res.status(200).json({
+            status: 'success',
+            token , 
+            message: 'Loggen in sucessfully',
+            user:{
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role
+
+            }
+
+        });
+    }catch(error) {
+        next(error);
+    }
 }
